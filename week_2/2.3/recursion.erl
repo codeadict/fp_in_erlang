7b233c6deca480d9ddf86a48fc312322a123b5bb
user.email <dairon@gooee.com>
2020-05-15 01:01:07 -0400

    Add fibonacci

--- week_2/2.3/recursion.erl
+++ week_2/2.3/recursion.erl
@@ -1,1 +1,13 @@
 -module(recursion).
+
+-export([fibonacci/1]).
+
+
+-spec fibonacci(N :: non_neg_integer()) -> pos_integer().
+fibonacci(1) ->
+    1;
+fibonacci(2) ->
+    2;
+fibonacci(N) when N > 2 ->
+    fibonacci(N - 2) + fibonacci(N - 1).
+
